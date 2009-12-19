2b2ecc72e60c3d79bfe6c0516a07faf1b0aadd54
Jonathan Hedley <jonathan@hedley.net>
2009-12-19 17:55:04 +1100

    Initial implementation of TokenStream.

--- src/test/java/org/jsoup/parser/TokenStreamTest.java
+++ src/test/java/org/jsoup/parser/TokenStreamTest.java
@@ -1,8 +1,21 @@
 package org.jsoup.parser;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+
 /**
- TODO: Document
+ TokenStream test cases.
 
  @author Jonathan Hedley, jonathan@hedley.net */
 public class TokenStreamTest {
+    @Test
+    public void createTokenStreamFromString() {
+        List<Token> tokens = TokenStream.create("<html><body><p>Hello world!</p></body></html>").asList();
+        assertEquals("Correct num tokens", 7, tokens.size());
+    }
+
+    // TODO: test data, positions, line breaks
 }
