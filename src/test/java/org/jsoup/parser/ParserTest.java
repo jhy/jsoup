b6e662acecbdb956e4775a5cef170840e6124c75
Jonathan Hedley <jonathan@hedley.net>
2009-12-23 16:34:45 +0800

    Updated TokenStream to deal with < or > within attributes.

--- src/test/java/org/jsoup/parser/ParserTest.java
+++ src/test/java/org/jsoup/parser/ParserTest.java
@@ -1,6 +1,7 @@
 package org.jsoup.parser;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -15,5 +16,20 @@ public class ParserTest {
         TokenStream tokenStream = TokenStream.create("<html><head><title>First!</title></head><body><p>First post! <img src=\"foo.png\" /></p></body></html>");
         Parser parser = new Parser(tokenStream);
         Document doc = parser.parse();
+        // need a better way to verify these:
+        Element p = doc.getChildren().get(1).getChildren().get(0);
+        assertEquals("p", p.getTagName());
+        assertEquals("foo.png", p.getChildren().get(0).getAttributes().get("src"));
+    }
+
+    @Test public void testParsesRoughAttributes() {
+        TokenStream tokenStream = TokenStream.create("<html><head><title>First!</title></head><body><p class=\"foo > bar\">First post! <img src=\"foo.png\" /></p></body></html>");
+        Parser parser = new Parser(tokenStream);
+        Document doc = parser.parse();
+        // need a better way to verify these:
+        Element p = doc.getChildren().get(1).getChildren().get(0);
+        assertEquals("p", p.getTagName());
+        assertEquals("foo > bar", p.getAttributes().get("class"));
+        assertEquals("foo.png", p.getChildren().get(0).getAttributes().get("src"));
     }
 }
