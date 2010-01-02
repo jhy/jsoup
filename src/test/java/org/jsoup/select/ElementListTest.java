7640aac0902f59feb9337d216ad001deb46fc3e3
Jonathan Hedley <jonathan@hedley.net>
2010-01-02 23:15:05 +1100

    Implemented ElementList.select()

---
+++ src/test/java/org/jsoup/select/ElementListTest.java
@@ -0,0 +1,21 @@
+package org.jsoup.select;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ Tests for ElementList.
+
+ @author Jonathan Hedley, jonathan@hedley.net */
+public class ElementListTest {
+    @Test public void chainedSelects() {
+        String h = "<p>Excl</p><div class=headline><p>Hello</p><p>There</p></div><div class=headline><h1>Headline</h1></div>";
+        Document doc = Jsoup.parse(h);
+        ElementList els = doc.select(".headline").select("p");
+        assertEquals(2, els.size());
+        assertEquals("Hello", els.get(0).text());
+        assertEquals("There", els.get(1).text());
+    }
+}
