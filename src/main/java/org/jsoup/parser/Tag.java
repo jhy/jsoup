5bfcf92e778c1fe33073f737daeecfbb09ef5fce
Jonathan Hedley <jonathan@hedley.net>
2010-01-04 23:33:00 +1100

    Fixed head canContain

--- src/main/java/org/jsoup/parser/Tag.java
+++ src/main/java/org/jsoup/parser/Tag.java
@@ -83,11 +83,10 @@ public class Tag {
             return false;
 
         // head can only contain a few. if more than head in here, modify to have a list of valids
-        // TODO[must] - lookup what head can contain
-        // from memory: base, script, link, meta, title
+        // (could solve this with walk for ancestor)
         if (this.tagName.equals("head")) {
             if (child.tagName.equals("base") || child.tagName.equals("script") || child.tagName.equals("link") ||
-                    child.tagName.equals("meta") || child.tagName.equals("title")) {
+                    child.tagName.equals("meta") || child.tagName.equals("title") || child.tagName.equals("style") || child.tagName.equals("object")) {
                 return true;
             }
             return false;
