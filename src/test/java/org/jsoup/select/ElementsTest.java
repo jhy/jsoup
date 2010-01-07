a48413eb1f7da2084d5bb11eead3e1d1d8c23e54
Jonathan Hedley <jonathan@hedley.net>
2010-01-07 19:29:52 +1100

    Attribute helpers in Element and Elements.

--- src/test/java/org/jsoup/select/ElementsTest.java
+++ src/test/java/org/jsoup/select/ElementsTest.java
@@ -18,4 +18,23 @@ public class ElementsTest {
         assertEquals("Hello", els.get(0).text());
         assertEquals("There", els.get(1).text());
     }
+
+    @Test public void attributes() {
+        String h = "<p title=foo><p title=bar><p class=foo><p class=bar>";
+        Document doc = Jsoup.parse(h);
+        Elements withTitle = doc.select("p[title]");
+        assertEquals(2, withTitle.size());
+        assertTrue(withTitle.hasAttr("title"));
+        assertFalse(withTitle.hasAttr("class"));
+        assertEquals("foo", withTitle.attr("title"));
+
+        withTitle.removeAttr("title");
+        assertEquals(2, withTitle.size()); // existing Elements are not reevaluated
+        assertEquals(0, doc.select("p[title]").size());
+
+        Elements ps = doc.select("p").attr("style", "classy");
+        assertEquals(4, ps.size());
+        assertEquals("classy", ps.last().attr("style"));
+        assertEquals("bar", ps.last().attr("class"));
+    }
 }
