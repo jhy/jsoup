2f39e48ab9a63cc5b418104119480411bb0e4140
Jonathan Hedley <jonathan@hedley.net>
2010-01-08 14:08:19 +1100

    Renamed Elements#select to Elements#filter.
    Renamed so the API is better self describing.

--- src/test/java/org/jsoup/select/ElementsTest.java
+++ src/test/java/org/jsoup/select/ElementsTest.java
@@ -10,10 +10,10 @@ import static org.junit.Assert.*;
 
  @author Jonathan Hedley, jonathan@hedley.net */
 public class ElementsTest {
-    @Test public void chainedSelects() {
+    @Test public void filter() {
         String h = "<p>Excl</p><div class=headline><p>Hello</p><p>There</p></div><div class=headline><h1>Headline</h1></div>";
         Document doc = Jsoup.parse(h);
-        Elements els = doc.select(".headline").select("p");
+        Elements els = doc.select(".headline").filter("p");
         assertEquals(2, els.size());
         assertEquals("Hello", els.get(0).text());
         assertEquals("There", els.get(1).text());
