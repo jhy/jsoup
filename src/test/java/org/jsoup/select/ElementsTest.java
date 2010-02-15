83ebe5f0d8ac65b355fe0250537b1f6a140c243a
Jonathan Hedley <jonathan@hedley.net>
2010-02-16 10:45:36 +1100

    Implemented Elements html(), html(string), append, and prepend.
    Closes #5.

--- src/test/java/org/jsoup/select/ElementsTest.java
+++ src/test/java/org/jsoup/select/ElementsTest.java
@@ -1,6 +1,7 @@
 package org.jsoup.select;
 
 import org.jsoup.Jsoup;
+import org.jsoup.TextUtil;
 import org.jsoup.nodes.Document;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -76,6 +77,29 @@ public class ElementsTest {
         Elements divs = doc.select("div");
         assertTrue(divs.hasText());
         assertFalse(doc.select("div + div").hasText());
+    }
+    
+    @Test public void html() {
+        Document doc = Jsoup.parse("<div><p>Hello</p></div><div><p>There</p></div>");
+        Elements divs = doc.select("div");
+        assertEquals("<p>Hello</p>\n<p>There</p>", divs.html());
+    }
+    
+    @Test public void outerHtml() {
+        Document doc = Jsoup.parse("<div><p>Hello</p></div><div><p>There</p></div>");
+        Elements divs = doc.select("div");
+        assertEquals("<div><p>Hello</p></div><div><p>There</p></div>", TextUtil.stripNewlines(divs.outerHtml()));
+    }
+    
+    @Test public void setHtml() {
+        Document doc = Jsoup.parse("<p>One</p><p>Two</p><p>Three</p>");
+        Elements ps = doc.select("p");
+        
+        ps.prepend("<b>Bold</b>").append("<i>Ital</i>");
+        assertEquals("<p><b>Bold</b>Two<i>Ital</i></p>", TextUtil.stripNewlines(ps.get(1).outerHtml()));
+        
+        ps.html("<span>Gone</span>");
+        assertEquals("<p><span>Gone</span></p>", TextUtil.stripNewlines(ps.get(1).outerHtml()));
     }
 
     @Test public void wrap() {
