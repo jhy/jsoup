bf456f4a0c14140ba4b69dd17e43130429e5069b
Jonathan Hedley <jonathan@hedley.net>
2010-08-28 11:50:18 +1000

    Treat HTTP headers as case insensitive in Jsoup.Connection. Improves compatibility for HTTP responses.

--- src/test/java/org/jsoup/helper/HttpConnectionTest.java
+++ src/test/java/org/jsoup/helper/HttpConnectionTest.java
@@ -1,10 +1,11 @@
 package org.jsoup.helper;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.jsoup.Connection;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class HttpConnectionTest {
     /* most actual network http connection tests are in integration */
@@ -22,5 +23,28 @@ public class HttpConnectionTest {
     @Test(expected=IllegalArgumentException.class) public void throwsExceptionOnBodyAsBytesWithoutExecute() throws IOException {
         Connection con = HttpConnection.connect("http://example.com");
         con.response().bodyAsBytes();
+    }
+
+    @Test public void caseInsensitiveHeaders() {
+        Connection.Response res = new HttpConnection.Response();
+        Map<String, String> headers = res.headers();
+        headers.put("Accept-Encoding", "gzip");
+        headers.put("content-type", "text/html");
+        headers.put("refErrer", "http://example.com");
+
+        assertTrue(res.hasHeader("Accept-Encoding"));
+        assertTrue(res.hasHeader("accept-encoding"));
+        assertTrue(res.hasHeader("accept-Encoding"));
+
+        assertEquals("gzip", res.header("accept-Encoding"));
+        assertEquals("text/html", res.header("Content-Type"));
+        assertEquals("http://example.com", res.header("Referrer"));
+
+        res.removeHeader("Content-Type");
+        assertFalse(res.hasHeader("content-type"));
+
+        res.header("accept-encoding", "deflate");
+        assertEquals("deflate", res.header("Accept-Encoding"));
+        assertEquals("deflate", res.header("accept-Encoding"));
     }
 }
