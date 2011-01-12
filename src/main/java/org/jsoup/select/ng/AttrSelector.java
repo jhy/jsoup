f8e487abd1681911e744986d66476448f66a2c41
Anton Kazennikov <kzn@iitp.ru>
2011-01-12 22:46:50 +0300

    Added attribute selectors

---
+++ src/main/java/org/jsoup/select/ng/AttrSelector.java
@@ -0,0 +1,96 @@
+package org.jsoup.select.ng;
+
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.Element;
+
+public class AttrSelector {
+
+	static class BaseAttrSelector {
+		String name;
+		String value;
+		public BaseAttrSelector(String name, String value) {
+			super();
+			this.name = name;
+			this.value = value;
+		}
+	}
+	
+	public static class AttrNameSelector extends BaseAttrSelector implements Selector {
+		public AttrNameSelector(String name) {
+			super(name, null);
+		}
+
+		@Override
+		public boolean select(Element element) {
+			return element.hasAttr(name);
+		}
+	}
+	
+	public static class AttrNameValueSelector extends BaseAttrSelector implements Selector {
+
+		public AttrNameValueSelector(String name, String value) {
+			super(name, value);
+		}
+
+		@Override
+		public boolean select(Element element) {
+			return element.attributes().get(name).equals(value);
+		}
+	}
+	
+	public static class AttrNameValuePrefixSelector extends BaseAttrSelector implements Selector {
+
+		public AttrNameValuePrefixSelector(String name, String value) {
+			super(name, value);
+		}
+
+		@Override
+		public boolean select(Element element) {
+			return element.attributes().get(name).startsWith(value);
+		}
+	}
+	
+	public static class AttrNameValueSuffixSelector extends BaseAttrSelector implements Selector {
+
+		public AttrNameValueSuffixSelector(String name, String value) {
+			super(name, value);
+		}
+
+		@Override
+		public boolean select(Element element) {
+			return element.attributes().get(name).endsWith(value);
+		}
+	}
+	
+	public static class AttrNameValueContainsSelector extends BaseAttrSelector implements Selector {
+
+		public AttrNameValueContainsSelector(String name, String value) {
+			super(name, value);
+		}
+
+		@Override
+		public boolean select(Element element) {
+			return element.attributes().get(name).contains(value);
+		}
+	}
+	
+	public static class AttrNameValueMatchesSelector extends BaseAttrSelector implements Selector {
+
+		public AttrNameValueMatchesSelector(String name, String value) {
+			super(name, value);
+		}
+
+		@Override
+		public boolean select(Element element) {
+			return element.attributes().get(name).matches(value);
+		}
+	}
+
+
+
+	
+	
+
+
+
+}
