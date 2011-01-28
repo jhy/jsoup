f317c04acf821d61601f5d986252db40505153bc
Jonathan Hedley <jonathan@hedley.net>
2011-01-28 14:31:19 +1100

    Force strict entity matching (must be &xxx; and not &xxx) in element attributes.
    Fixes #71

--- src/test/java/org/jsoup/parser/AttributeParseTest.java
+++ src/test/java/org/jsoup/parser/AttributeParseTest.java
@@ -3,6 +3,7 @@ package org.jsoup.parser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -41,5 +42,12 @@ public class AttributeParseTest {
         Element el = Jsoup.parse(html).getElementsByTag("a").get(0);
         Attributes attr = el.attributes();
         assertEquals(0, attr.size());
+    }
+
+    @Test public void strictAttributeUnescapes() {
+        String html = "<a id=1 href='?foo=bar&mid&lt=true'>One</a> <a id=2 href='?foo=bar&lt;qux&lg=1'>Two</a>";
+        Elements els = Jsoup.parse(html).select("a");
+        assertEquals("?foo=bar&mid&lt=true", els.first().attr("href"));
+        assertEquals("?foo=bar<qux&lg=1", els.last().attr("href"));
     }
 }
