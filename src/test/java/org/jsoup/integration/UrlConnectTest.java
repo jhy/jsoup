800d6e12a38c937083cee0befa3319b447dd8886
Jonathan Hedley <jonathan@hedley.net>
2011-02-03 21:48:41 +1100

    Don't run URL connectivity tests by default.

--- src/test/java/org/jsoup/integration/UrlConnectTest.java
+++ src/test/java/org/jsoup/integration/UrlConnectTest.java
@@ -14,7 +14,7 @@ import java.io.IOException;
  Tests the URL connection. Not enabled by default, so tests don't require network connection.
 
  @author Jonathan Hedley, jonathan@hedley.net */
-//@Ignore // ignored by default so tests don't require network access. comment out to enable.
+@Ignore // ignored by default so tests don't require network access. comment out to enable.
 public class UrlConnectTest {
     private static String echoURL = "http://infohound.net/tools/q.pl";
 
