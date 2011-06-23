a74e16f335a8854f8c2ba421e06daacf7e0b211b
Jonathan Hedley <jonathan@hedley.net>
2011-06-23 21:48:42 +1000

    Fixed normalisation of carriage returns to newlines on input HTML
    Fixes #110

--- src/test/java/org/jsoup/parser/CharacterReaderTest.java
+++ src/test/java/org/jsoup/parser/CharacterReaderTest.java
@@ -60,4 +60,21 @@ public class CharacterReaderTest {
         r.rewindToMark();
         assertEquals('n', r.consume());
     }
+
+    @Test public void handleCarriageReturnAsLineFeed() {
+        String in = "one \r two \r\n three";
+        CharacterReader r = new CharacterReader(in);
+
+        String first = r.consumeTo('\n');
+        assertEquals("one ", first);
+        assertEquals("\n two \n three", r.consumeToEnd());
+    }
+
+    @Test public void consumeToEnd() {
+        String in = "one two three";
+        CharacterReader r = new CharacterReader(in);
+        String toEnd = r.consumeToEnd();
+        assertEquals(in, toEnd);
+        assertTrue(r.isEmpty());
+    }
 }
