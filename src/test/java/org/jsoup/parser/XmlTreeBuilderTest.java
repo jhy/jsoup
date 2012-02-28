889ff7cfb605adf5b692e3740294bcdc11b14e48
Jonathan Hedley <jonathan@hedley.net>
2012-02-27 19:06:03 -0800

    Added test coverage for XML parser.

--- src/test/java/org/jsoup/parser/XmlTreeBuilderTest.java
+++ src/test/java/org/jsoup/parser/XmlTreeBuilderTest.java
@@ -19,5 +19,25 @@ public class XmlTreeBuilderTest {
         Document doc = tb.parse(xml, "http://foo.com/");
         assertEquals("<doc id=\"2\" href=\"/bar\">Foo <br /><link>One</link><link>Two</link></doc>",
                 TextUtil.stripNewlines(doc.html()));
+        assertEquals(doc.getElementById("2").absUrl("href"), "http://foo.com/bar");
+    }
+
+    @Test
+    public void testPopToClose() {
+        // test: </val> closes Two, </bar> ignored
+        String xml = "<doc><val>One<val>Two</val></bar>Three</doc>";
+        XmlTreeBuilder tb = new XmlTreeBuilder();
+        Document doc = tb.parse(xml, "http://foo.com/");
+        assertEquals("<doc><val>One<val>Two</val>Three</val></doc>",
+                TextUtil.stripNewlines(doc.html()));
+    }
+
+    @Test
+    public void testCommentAndDocType() {
+        String xml = "<!DOCTYPE html><!-- a comment -->One <qux />Two";
+        XmlTreeBuilder tb = new XmlTreeBuilder();
+        Document doc = tb.parse(xml, "http://foo.com/");
+        assertEquals("<!DOCTYPE html><!-- a comment -->One <qux />Two",
+                TextUtil.stripNewlines(doc.html()));
     }
 }
