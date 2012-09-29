3cb4cee19e3cb6830735a0f89d890576c7413f2c
Jonathan Hedley <jonathan@hedley.net>
2012-09-29 13:31:32 -0700

    Allow Whitelist test methods to be extended
    Fixes #85

--- src/main/java/org/jsoup/safety/Whitelist.java
+++ src/main/java/org/jsoup/safety/Whitelist.java
@@ -319,10 +319,22 @@ public class Whitelist {
         return this;
     }
 
-    boolean isSafeTag(String tag) {
+    /**
+     * Test if the supplied tag is allowed by this whitelist
+     * @param tag test tag
+     * @return true if allowed
+     */
+    protected boolean isSafeTag(String tag) {
         return tagNames.contains(TagName.valueOf(tag));
     }
 
+    /**
+     * Test if the supplied attribute is allowed by this whitelist for this tag
+     * @param tagName tag to consider allowing the attribute in
+     * @param el element under test, to confirm protocol
+     * @param attr attribute under test
+     * @return true if allowed
+     */
     boolean isSafeAttribute(String tagName, Element el, Attribute attr) {
         TagName tag = TagName.valueOf(tagName);
         AttributeKey key = AttributeKey.valueOf(attr.getKey());
