8005ee7fe19d0ed6fd86daf35ed562baef0f9873
Michael J. Simons <michael@simons.ac>
2012-10-15 07:12:36 +0200

    Correct checking for surrogate pairs

--- src/main/java/org/jsoup/nodes/Entities.java
+++ src/main/java/org/jsoup/nodes/Entities.java
@@ -84,7 +84,7 @@ public class Entities {
 
         for (int pos = 0; pos < string.length(); pos++) {
             Character c = string.charAt(pos);
-			if(Character.isHighSurrogate(c) && pos < string.length() - 1 && Character.isLowSurrogate(string.charAt(pos+1)))
+			if(pos < string.length() - 1 && Character.isSurrogatePair(c, string.charAt(pos+1)))
 				accum.append(new String(new int[]{c, string.charAt(++pos)}, 0, 2));
             else if (map.containsKey(c))
                 accum.append('&').append(map.get(c)).append(';');
