02952110d5525e78c2ea496f258c310714937dc8
Luke Farrar <luke.farrar@thebookpeople.co.uk>
2012-11-08 11:52:01 +0000

    Failing tests for simple html (self-closing newlines).

--- src/test/java/org/jsoup/parser/HtmlParserTest.java
+++ src/test/java/org/jsoup/parser/HtmlParserTest.java
@@ -741,11 +741,22 @@ public class HtmlParserTest {
         Document doc = Jsoup.parse(html);
         assertEquals("<!--?xml encoding='UTF-8' ?--> <html> <head></head> <body> One </body> </html>", StringUtil.normaliseWhitespace(doc.outerHtml()));
     }
-    
-    @Test
-    public void handlesTrailingNewlines() {
+  
+  	@Test public void handlesTrailingNewlines() {
         String html = "<html><body><div><p><a id=\"theId\" /></p></div></body></html>\n";
         Document doc = Jsoup.parse(html);
         assertEquals("<html> <head></head> <body> <div> <p><a id=\"theId\"></a></p> </div> </body> </html>", StringUtil.normaliseWhitespace(doc.outerHtml()));
+    }
+    
+    @Test public void handlesSelfClosingAnchors() {
+        String html = "<html><body><div><p><a id=\"theId\" />foo.</p></div></body></html>";
+        Document doc = Jsoup.parse(html);
+        assertEquals("<html> <head></head> <body> <div> <p><a id=\"theId\"></a>foo.</p> </div> </body> </html>", StringUtil.normaliseWhitespace(doc.outerHtml()));
+    } 
+
+    @Test public void handlesRogueNewlines() {
+        String html = "<html><body><div><p><a id=\"theId\" />foo</p>\n</div></body></html>";
+        Document doc = Jsoup.parse(html);
+        assertEquals("<html> <head></head> <body> <div> <p><a id=\"theId\"></a>foo</p> </div> </body> </html>", StringUtil.normaliseWhitespace(doc.outerHtml()));
     }
 }
