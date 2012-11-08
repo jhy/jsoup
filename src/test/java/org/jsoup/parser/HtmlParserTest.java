768862e60aae353ff1518196778dfb7724ac4f78
Luke Farrar <luke.farrar@thebookpeople.co.uk>
2012-11-08 11:49:38 +0000

    Unused import.

--- src/test/java/org/jsoup/parser/HtmlParserTest.java
+++ src/test/java/org/jsoup/parser/HtmlParserTest.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 /**
