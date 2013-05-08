4a052c178816afe85659cf5934d55e1d5f0ed81b
fabse <fab@fabiankoehler.de>
2013-05-08 13:25:34 +0200

    add extra case where the charset is in an extra argument

--- src/main/java/org/jsoup/helper/DataUtil.java
+++ src/main/java/org/jsoup/helper/DataUtil.java
@@ -81,7 +81,23 @@ public class DataUtil {
             doc = parser.parseInput(docData, baseUri);
             Element meta = doc.select("meta[http-equiv=content-type], meta[charset]").first();
             if (meta != null) { // if not found, will keep utf-8 as best attempt
-                String foundCharset = meta.hasAttr("http-equiv") ? getCharsetFromContentType(meta.attr("content")) : meta.attr("charset");
+
+                String foundCharset;
+                if (meta.hasAttr("http-equiv")) {
+                    foundCharset = getCharsetFromContentType(meta.attr("content"));
+                    if (foundCharset == null && meta.hasAttr("charset")) {
+                        try {
+                            if (Charset.isSupported(meta.attr("charset"))) {
+                                foundCharset = meta.attr("charset");
+                            }
+                        } catch (IllegalCharsetNameException e) {
+                            foundCharset = null;
+                        }
+                    }
+                } else {
+                    foundCharset = meta.attr("charset");
+                }
+
                 if (foundCharset != null && foundCharset.length() != 0 && !foundCharset.equals(defaultCharset)) { // need to re-decode
                     foundCharset = foundCharset.trim().replaceAll("[\"']", "");
                     charsetName = foundCharset;
