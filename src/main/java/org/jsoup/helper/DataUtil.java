c3cbe1b64e7f66ff9f9b53f1388eb135e6693187
Jonathan Hedley <jonathan@hedley.net>
2016-04-05 14:03:04 -0700

    Support UTF-16 and -32 BOMs
    Fixes #695

--- src/main/java/org/jsoup/helper/DataUtil.java
+++ src/main/java/org/jsoup/helper/DataUtil.java
@@ -8,10 +8,10 @@ import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.IllegalCharsetNameException;
+import java.util.Locale;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.Locale;
 
 /**
  * Internal static utilities for handling data.
@@ -88,6 +88,23 @@ public final class DataUtil {
     static Document parseByteData(ByteBuffer byteData, String charsetName, String baseUri, Parser parser) {
         String docData;
         Document doc = null;
+
+        // look for BOM - overrides any other header or input
+        byteData.mark();
+        byte[] bom = new byte[4];
+        byteData.get(bom);
+        byteData.rewind();
+        if (bom[0] == 0x00 && bom[1] == 0x00 && bom[2] == (byte) 0xFE && bom[3] == (byte) 0xFF || // BE
+                bom[0] == (byte) 0xFF && bom[1] == (byte) 0xFE && bom[2] == 0x00 && bom[3] == 0x00) { // LE
+            charsetName = "UTF-32"; // and I hope it's on your system
+        } else if (bom[0] == (byte) 0xFE && bom[1] == (byte) 0xFF || // BE
+                bom[0] == (byte) 0xFF && bom[1] == (byte) 0xFE) {
+            charsetName = "UTF-16"; // in all Javas
+        } else if (bom[0] == (byte) 0xEF && bom[1] == (byte) 0xBB && bom[2] == (byte) 0xBF) {
+            charsetName = "UTF-8"; // in all Javas
+            byteData.position(3); // 16 and 32 decoders consume the BOM to determine be/le; utf-8 should be consumed
+        }
+
         if (charsetName == null) { // determine from meta. safe parse as UTF-8
             // look for <meta http-equiv="Content-Type" content="text/html;charset=gb2312"> or HTML5 <meta charset="gb2312">
             docData = Charset.forName(defaultCharset).decode(byteData).toString();
@@ -119,14 +136,6 @@ public final class DataUtil {
         } else { // specified by content type header (or by user on file load)
             Validate.notEmpty(charsetName, "Must set charset arg to character set of file to parse. Set to null to attempt to detect from HTML");
             docData = Charset.forName(charsetName).decode(byteData).toString();
-        }
-        // UTF-8 BOM indicator. takes precedence over everything else. rarely used. re-decodes incase above decoded incorrectly
-        if (docData.length() > 0 && docData.charAt(0) == UNICODE_BOM) {
-            byteData.rewind();
-            docData = Charset.forName(defaultCharset).decode(byteData).toString();
-            docData = docData.substring(1);
-            charsetName = defaultCharset;
-            doc = null;
         }
         if (doc == null) {
             doc = parser.parseInput(docData, baseUri);
--- src/test/java/org/jsoup/helper/DataUtilTest.java
+++ src/test/java/org/jsoup/helper/DataUtilTest.java
@@ -1,6 +1,10 @@
 package org.jsoup.helper;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.parser.Parser;
 import org.junit.Test;
@@ -8,6 +12,7 @@ import org.junit.Test;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 
+import static org.jsoup.integration.ParseTest.getFile;
 import static org.junit.Assert.*;
 
 public class DataUtilTest {
@@ -94,5 +99,29 @@ public class DataUtilTest {
         } catch( UnsupportedEncodingException ex ) {
             fail(ex.getMessage());
         }
+    }
+
+    @Test
+    public void supportsBOMinFiles() throws IOException {
+        // test files from http://www.i18nl10n.com/korean/utftest/
+        File in = getFile("/bomtests/bom_utf16be.html");
+        Document doc = Jsoup.parse(in, null, "http://example.com");
+        assertTrue(doc.title().contains("UTF-16BE"));
+        assertTrue(doc.text().contains("가각갂갃간갅"));
+
+        in = getFile("/bomtests/bom_utf16le.html");
+        doc = Jsoup.parse(in, null, "http://example.com");
+        assertTrue(doc.title().contains("UTF-16LE"));
+        assertTrue(doc.text().contains("가각갂갃간갅"));
+
+        in = getFile("/bomtests/bom_utf32be.html");
+        doc = Jsoup.parse(in, null, "http://example.com");
+        assertTrue(doc.title().contains("UTF-32BE"));
+        assertTrue(doc.text().contains("가각갂갃간갅"));
+
+        in = getFile("/bomtests/bom_utf32le.html");
+        doc = Jsoup.parse(in, null, "http://example.com");
+        assertTrue(doc.title().contains("UTF-32LE"));
+        assertTrue(doc.text().contains("가각갂갃간갅"));
     }
 }
