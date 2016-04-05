07d990fe2aa7f2991d5b74f96ea2de690514338b
Jonathan Hedley <jonathan@hedley.net>
2016-04-05 11:53:20 -0700

    Removed unused change from PR

--- src/main/java/org/jsoup/select/NodeTraversor.java
+++ src/main/java/org/jsoup/select/NodeTraversor.java
@@ -22,9 +22,8 @@ public class NodeTraversor {
     /**
      * Start a depth-first traverse of the root and all of its descendants.
      * @param root the root node point to traverse.
-     * @param namespace the namespace of the document
      */
-    public void traverse(Node root, String namespace) {
+    public void traverse(Node root) {
         Node node = root;
         int depth = 0;
         
@@ -45,10 +44,5 @@ public class NodeTraversor {
                 node = node.nextSibling();
             }
         }
-    }
-    
-    public void traverse (Node root)
-    {
-    	traverse (root, null);
     }
 }
