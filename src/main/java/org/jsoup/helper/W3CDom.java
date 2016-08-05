f148f88de365c50eea28d3e14093e13c33104483
Jonathan Hedley <jonathan@hedley.net>
2016-08-05 16:41:40 -0700

    Normalize invalid attribute names in XML when converting
    Fixes #721

--- src/main/java/org/jsoup/helper/W3CDom.java
+++ src/main/java/org/jsoup/helper/W3CDom.java
@@ -121,7 +121,10 @@ public class W3CDom {
 
         private void copyAttributes(org.jsoup.nodes.Node source, Element el) {
             for (Attribute attribute : source.attributes()) {
-                el.setAttribute(attribute.getKey(), attribute.getValue());
+                // valid xml attribute names are: ^[a-zA-Z_:][-a-zA-Z0-9_:.]
+                String key = attribute.getKey().replaceAll("[^-a-zA-Z0-9_:.]", "");
+                if (key.matches("[a-zA-Z_:]{1}[-a-zA-Z0-9_:.]*"))
+                    el.setAttribute(key, attribute.getValue());
             }
         }
 
