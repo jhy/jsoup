040ce712b73fcb3ff16b7d8dda1246ccd23a6988
Jonathan Hedley <jonathan@hedley.net>
2017-01-06 09:46:11 -0800

    Javadoc tweaks

--- src/main/java/org/jsoup/Connection.java
+++ src/main/java/org/jsoup/Connection.java
@@ -84,7 +84,11 @@ public interface Connection {
 
     /**
      * Set the request timeouts (connect and read). If a timeout occurs, an IOException will be thrown. The default
-     * timeout is <b<30 seconds</b> (30000 millis). A timeout of zero is treated as an infinite timeout.
+     * timeout is <b>30 seconds</b> (30,000 millis). A timeout of zero is treated as an infinite timeout.
+     * <p>Note that a read timeout is not the same as a maximum timeout. As long as the connection is sending bytes at
+     * least every <i>timeout</i> seconds (e.g. in the case of an infinite stream of data, or a slow large download), the
+     * read timeout will not fire. This can be mitigated by using a maximum download size (see {@link #maxBodySize(int)}),
+     * or interrupting the connecting thread after a max timeout.</p>
      * @param millis number of milliseconds (thousandths of a second) before timing out connects or reads.
      * @return this Connection, for chaining
      */
--- src/main/java/org/jsoup/parser/TokenQueue.java
+++ src/main/java/org/jsoup/parser/TokenQueue.java
@@ -250,7 +250,7 @@ public class TokenQueue {
 
     /**
      * Pulls a balanced string off the queue. E.g. if queue is "(one (two) three) four", (,) will return "one (two) three",
-     * and leave " four" on the queue. Unbalanced openers and closers can quoted (with ' or ") or escaped (with \). Those escapes will be left
+     * and leave " four" on the queue. Unbalanced openers and closers can be quoted (with ' or ") or escaped (with \). Those escapes will be left
      * in the returned string, which is suitable for regexes (where we need to preserve the escape), but unsuitable for
      * contains text strings; use unescape for that.
      * @param open opener
