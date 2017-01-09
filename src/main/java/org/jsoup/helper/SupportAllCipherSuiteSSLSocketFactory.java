5c442464075bf4f6f7902ed93987a0da17798766
zhoulw <645412488@qq.com>
2017-01-09 14:48:12 +0800

    support what jdk support

--- src/main/java/org/jsoup/helper/SupportAllCipherSuiteSSLSocketFactory.java
+++ src/main/java/org/jsoup/helper/SupportAllCipherSuiteSSLSocketFactory.java
@@ -83,23 +83,6 @@ public class SupportAllCipherSuiteSSLSocketFactory extends SSLSocketFactory {
 		String[] defaultCipherSuites = sslSocketFactory.getDefaultCipherSuites();
 
 		ArrayList<String> suitesList = new ArrayList<String>(Arrays.asList(defaultCipherSuites));
-		suitesList.add("TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256");
-		suitesList.add("TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256");
-		//suitesList.add("TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384");
-		//suitesList.add("TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384");
-		//suitesList.add("TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256");
-		//suitesList.add("TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256");
-		
-		suitesList.add("TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA");
-		suitesList.add("TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA");
-		//suitesList.add("TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA");
-		//suitesList.add("TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA");
-		//suitesList.add("TLS_RSA_WITH_AES_128_GCM_SHA256");
-		//suitesList.add("TLS_RSA_WITH_AES_256_GCM_SHA384");
-		//suitesList.add("TLS_RSA_WITH_AES_128_CBC_SHA");
-		//suitesList.add("TLS_RSA_WITH_AES_256_CBC_SHA");
-		//suitesList.add("TLS_RSA_WITH_3DES_EDE_CBC_SHA");
-
 		return suitesList.toArray(new String[suitesList.size()]);
 	}
 }
