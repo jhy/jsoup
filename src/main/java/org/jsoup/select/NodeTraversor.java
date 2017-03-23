7126fdab01b39709dbc73c014d61c39bd8daaf37
Erich Schubert <schubert@informatik.uni-heidelberg.de>
2017-03-23 17:18:27 +0100

    Add new NodeFilter interface to NodeTraversor

--- src/main/java/org/jsoup/select/NodeTraversor.java
+++ src/main/java/org/jsoup/select/NodeTraversor.java
@@ -1,6 +1,9 @@
 package org.jsoup.select;
 
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
+import org.jsoup.select.NodeFilter.FilterResult;
 
 /**
  * Depth-first node traversor. Use to iterate through all nodes under and including the specified root node.
@@ -65,5 +68,70 @@ public class NodeTraversor {
         Validate.notNull(elements);
         for (Element el : elements)
             traverse(visitor, el);
+    }
+
+    /**
+     * Start a depth-first filtering of the root and all of its descendants.
+     * @param filter Node visitor.
+     * @param root the root node point to traverse.
+     * @return The filter result of the root node, or {@link FilterResult#STOP}.
+     */
+    public static FilterResult filter(NodeFilter filter, Node root) {
+        Node node = root;
+        int depth = 0;
+
+        while (node != null) {
+            FilterResult result = filter.head(node, depth);
+            if (result == FilterResult.STOP)
+                return result;
+            // Descend into child nodes:
+            if (result == FilterResult.CONTINUE && node.childNodeSize() > 0) {
+                node = node.childNode(0);
+                ++depth;
+                continue;
+            }
+            // No siblings, move upwards:
+            while (node.nextSibling() == null && depth > 0) {
+                // 'tail' current node:
+                if (result == FilterResult.CONTINUE || result == FilterResult.SKIP_CHILDREN) {
+                    result = filter.tail(node, depth);
+                    if (result == FilterResult.STOP)
+                        return result;
+                }
+                Node prev = node; // In case we need to remove it below.
+                node = node.parentNode();
+                depth--;
+                if (result == FilterResult.REMOVE)
+                    prev.remove(); // Remove AFTER finding parent.
+                result = FilterResult.CONTINUE; // Parent was not pruned.
+            }
+            // 'tail' current node, then proceed with siblings:
+            if (result == FilterResult.CONTINUE || result == FilterResult.SKIP_CHILDREN) {
+                result = filter.tail(node, depth);
+                if (result == FilterResult.STOP)
+                    return result;
+            }
+            if (node == root)
+                return result;
+            Node prev = node; // In case we need to remove it below.
+            node = node.nextSibling();
+            if (result == FilterResult.REMOVE)
+                prev.remove(); // Remove AFTER finding sibling.
+        }
+        // root == null?
+        return FilterResult.CONTINUE;
+    }
+
+    /**
+     * Start a depth-first filtering of all elements.
+     * @param filter Node filter.
+     * @param elements Elements to filter.
+     */
+    public static void filter(NodeFilter filter, Elements elements) {
+        Validate.notNull(filter);
+        Validate.notNull(elements);
+        for (Element el : elements)
+            if (filter(filter, el) == FilterResult.STOP)
+                break;
     }
 }
