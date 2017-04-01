885c39c66a7edca51edfab582946bbfb450af06c
Woojin Lee <holinder4s@gmail.com>
2017-04-01 18:45:32 +0900

    Attribute.java TestCase : Add Test about equals method(coverage 74.5 to 77.4)

--- src/test/java/testoracle/AttributeTest.java
+++ src/test/java/testoracle/AttributeTest.java
@@ -54,6 +54,25 @@ public class AttributeTest {
 	*/
 	
 	@Test
+	public void testEqualsObject() {
+		Attribute a = new Attribute("Tot", "a&p");
+		Attribute aKeyValueEqual = new Attribute("Tot", "a&p");
+		Attribute aOnlyKeyEqual = new Attribute("Tot", "wjdebug");
+		Attribute aOnlyValueEqual = new Attribute("Hello", "a&p");
+		Attribute aNotEqual = new Attribute("Hello", "There");
+		Attributes as = new Attributes();
+		as.put("Tot", "a&p");
+		
+		assertTrue(a.equals(a));				// Case1 : Compare with self
+		assertTrue(a.equals(aKeyValueEqual));	// Case2 : Compare with Key,Value Equal Attribute
+		assertFalse(a.equals(aOnlyKeyEqual));	// Case3 : Compare with only Key Equal Attribute
+		assertFalse(a.equals(aOnlyValueEqual));	// Case4 : Compare with only Value Equal Attribute
+		assertFalse(a.equals(aNotEqual));		// Case5 : Compare with not equal Attribute
+		
+		assertFalse(a.equals(as));				// Case6 : Compare with Attributes
+	}
+	
+	@Test
 	public void testHashCode() {
 		fail("Not yet implemented");
 	}
@@ -85,11 +104,6 @@ public class AttributeTest {
 
 	@Test
 	public void testIsBooleanAttribute() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testEqualsObject() {
 		fail("Not yet implemented");
 	}
 
