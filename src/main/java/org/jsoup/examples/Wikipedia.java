1b38f80d623310e84c87b2d708ed0ab648dfafc5
Jonathan Hedley <jonathan@hedley.net>
2017-11-04 18:11:21 -0700

    Added sample from site

---
+++ src/main/java/org/jsoup/examples/Wikipedia.java
@@ -0,0 +1,27 @@
+package org.jsoup.examples;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+/**
+ * A simple example, used on the jsoup website.
+ */
+public class Wikipedia {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
+        log(doc.title());
+
+        Elements newsHeadlines = doc.select("#mp-itn b a");
+        for (Element headline : newsHeadlines) {
+            log("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
+        }
+    }
+
+    private static void log(String msg, String... vals) {
+        System.out.println(String.format(msg, vals));
+    }
+}
