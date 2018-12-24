1d663eeb1c0d1c08bcfe5d30f05170768bb0438e
Jonathan Hedley <jonathan@hedley.net>
2018-12-23 22:27:31 -0800

    Don't NPE in setValue for an orphan Attribute
    Fixes #1107

--- src/main/java/org/jsoup/nodes/Attribute.java
+++ src/main/java/org/jsoup/nodes/Attribute.java
@@ -85,14 +85,15 @@ public class Attribute implements Map.Entry<String, String>, Cloneable  {
      @param val the new attribute value; must not be null
      */
     public String setValue(String val) {
-        String oldVal = parent.get(this.key);
+        String oldVal = this.val;
         if (parent != null) {
+            oldVal = parent.get(this.key); // trust the container more
             int i = parent.indexOfKey(this.key);
             if (i != Attributes.NotFound)
                 parent.vals[i] = val;
         }
         this.val = val;
-        return oldVal;
+        return Attributes.checkNotNull(oldVal);
     }
 
     /**
--- src/test/java/org/jsoup/nodes/AttributeTest.java
+++ src/test/java/org/jsoup/nodes/AttributeTest.java
@@ -37,4 +37,14 @@ public class AttributeTest {
         assertEquals("hidden", first.getKey());
         assertEquals("", first.getValue());
     }
+
+    @Test public void settersOnOrphanAttribute() {
+        Attribute attr = new Attribute("one", "two");
+        attr.setKey("three");
+        String oldVal = attr.setValue("four");
+        assertEquals("two", oldVal);
+        assertEquals("three", attr.getKey());
+        assertEquals("four", attr.getValue());
+        assertEquals(null, attr.parent);
+    }
 }
