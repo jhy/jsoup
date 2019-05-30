dd2f39193e98b6d9bb45dd2cc1d6e3cdea55ca29
Čedomir Igaly <cedomir.igaly@altima.hr>
2019-05-30 09:27:35 +0200

    Small cleaning.

--- src/main/java/org/jsoup/helper/W3CDom.java
+++ src/main/java/org/jsoup/helper/W3CDom.java
@@ -246,7 +246,6 @@ public class W3CDom {
                 // valid xml attribute names are: ^[a-zA-Z_:][-a-zA-Z0-9_:.]
                 String key = attribute.getKey().replaceAll("[^-a-zA-Z0-9_:.]", "");
                 if (key.matches("[a-zA-Z_:][-a-zA-Z0-9_:.]*"))
-                    //el.setAttribute(key, attribute.getValue());
                     el.setAttribute(key.toLowerCase(), attribute.getValue());
             }
         }
