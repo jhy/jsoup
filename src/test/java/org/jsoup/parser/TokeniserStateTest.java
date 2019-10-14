f110f2812e0cd9a483336d1e01b41fa6fa7aefe5
Csaba Varga <csaboka@gmail.com>
2019-10-14 08:05:38 +0200

    Fixed some line ending and whitespace issues.

--- src/test/java/org/jsoup/parser/TokeniserStateTest.java
+++ src/test/java/org/jsoup/parser/TokeniserStateTest.java
@@ -206,7 +206,7 @@ public class TokeniserStateTest {
         Document doc = Jsoup.parse(html);
         assertEquals("<p></p><p></p><div id=\"one\"><span>Two</span></div>", TextUtil.stripNewlines(doc.body().html()));
     }
-    
+
     @Test
     public void testUnconsumeAtBufferBoundary() {
         String triggeringSnippet = "<a href=\"\"foo";
@@ -214,9 +214,9 @@ public class TokeniserStateTest {
         Arrays.fill(padding, ' ');
         String paddedSnippet = new StringBuilder().append(padding).append(triggeringSnippet).toString();
         ParseErrorList errorList = ParseErrorList.tracking(1);
-        
+
         Parser.parseFragment(paddedSnippet, null, "", errorList);
-        
+
         assertEquals(CharacterReader.readAheadLimit - 1, errorList.get(0).getPosition());
     }
     
@@ -224,19 +224,19 @@ public class TokeniserStateTest {
     public void testOpeningAngleBracketInsteadOfAttribute() {
         String triggeringSnippet = "<html <";
         ParseErrorList errorList = ParseErrorList.tracking(1);
-        
+
         Parser.parseFragment(triggeringSnippet, null, "", errorList);
-        
+
         assertEquals(6, errorList.get(0).getPosition());
     }
-    
+
     @Test
     public void testMalformedSelfClosingTag() {
         String triggeringSnippet = "<html /ouch";
         ParseErrorList errorList = ParseErrorList.tracking(1);
-        
+
         Parser.parseFragment(triggeringSnippet, null, "", errorList);
-        
+
         assertEquals(7, errorList.get(0).getPosition());
     }
 
@@ -244,9 +244,9 @@ public class TokeniserStateTest {
     public void testOpeningAngleBracketInTagName() {
         String triggeringSnippet = "<html<";
         ParseErrorList errorList = ParseErrorList.tracking(1);
-        
+
         Parser.parseFragment(triggeringSnippet, null, "", errorList);
-        
+
         assertEquals(5, errorList.get(0).getPosition());
     }
 }
