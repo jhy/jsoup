c5f83d0f3a15e7a0d36c48c34c1d3f5eeac0a63b
HcPlu <HcPlu@outlook.com>
2020-05-04 15:55:47 +0800

    1024

--- src/main/java/org/jsoup/examples/test/cpp.java
+++ src/main/java/org/jsoup/examples/test/cpp.java
@@ -1,4 +1,23 @@
 package org.jsoup.examples.test;
 
-public class cpp {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class cpp {public static void main(String[] args) throws IOException {
+    final Document doc = Jsoup.parse("<div> )foo</div> \n<div>y foo</div>\n<div>\\)</div>\n<div>1</div>");
+
+//        System.out.println(doc.select("div:matches(" + "1" + ")"));
+    System.out.println(doc.select("div:matches(" + "\\)" + ")"));
+
+//        System.out.println(doc.select("div:matches(" + Pattern.quote("*") + ")"));
+
+
+
+
+//       System.out.println(doc.select("div:matches(" +"" + "\\)"+")"));
+
+
+}
 }
