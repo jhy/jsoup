jhy/jsoup

commit a657ae0240b875a703a1e4909d56ad59997d362d

Added forms(), comments(), textNodes(), dataNodes()

src/main/java/org/jsoup/select/Elements.java
@@ -1,10 +1,13 @@
 package org.jsoup.select;
 
-import org.jsoup.internal.StringUtil;
 import org.jsoup.helper.Validate;
+import org.jsoup.internal.StringUtil;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.FormElement;
 import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -633,11 +636,48 @@ public class Elements extends ArrayList<Element> {
      * no forms.
      */
     public List<FormElement> forms() {
-        ArrayList<FormElement> forms = new ArrayList<>();
-        for (Element el: this)
-            if (el instanceof FormElement)
-                forms.add((FormElement) el);
-        return forms;
+        return nodesOfType(FormElement.class);
+    }
+
+    /**
+     * Get {@link Comment} nodes that are direct child nodes of the selected elements.
+     * @return Comment nodes, or an empty list if none.
+     */
+    public List<Comment> comments() {
+        return nodesOfType(Comment.class);
+    }
+
+    /**
+     * Get {@link TextNode} nodes that are direct child nodes of the selected elements.
+     * @return TextNode nodes, or an empty list if none.
+     */
+    public List<TextNode> textNodes() {
+        return nodesOfType(TextNode.class);
+    }
+
+    /**
+     * Get {@link DataNode} nodes that are direct child nodes of the selected elements. DataNode nodes contain the
+     * content of tags such as {@code script}, {@code style} etc and are distinct from {@link TextNode}s.
+     * @return Comment nodes, or an empty list if none.
+     */
+    public List<DataNode> dataNodes() {
+        return nodesOfType(DataNode.class);
+    }
+
+    private <T extends Node> List<T> nodesOfType(Class<T> tClass) {
+        ArrayList<T> nodes = new ArrayList<>();
+        for (Element el: this) {
+            if (el.getClass().isInstance(tClass)) { // Handles FormElements
+                nodes.add(tClass.cast(el));
+            } else if (Node.class.isAssignableFrom(tClass)) { // check if child nodes match
+                for (int i = 0; i < el.childNodeSize(); i++) {
+                    Node node = el.childNode(i);
+                    if (tClass.isInstance(node))
+                        nodes.add(tClass.cast(node));
+                }
+            }
+        }
+        return nodes;
     }
 
 }

src/test/java/org/jsoup/select/ElementsTest.java
@@ -2,17 +2,18 @@ package org.jsoup.select;
 
 import org.jsoup.Jsoup;
 import org.jsoup.TextUtil;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.FormElement;
 import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
 import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  Tests for ElementList.
@@ -296,6 +297,55 @@ public class ElementsTest {
         assertTrue(forms.get(1) != null);
         assertEquals("1", forms.get(0).id());
         assertEquals("2", forms.get(1).id());
+    }
+
+    @Test public void comments() {
+        Document doc = Jsoup.parse("<!-- comment1 --><p><!-- comment2 --><p class=two><!-- comment3 -->");
+        List<Comment> comments = doc.select("p").comments();
+        assertEquals(2, comments.size());
+        assertEquals(" comment2 ", comments.get(0).getData());
+        assertEquals(" comment3 ", comments.get(1).getData());
+
+        List<Comment> comments1 = doc.select("p.two").comments();
+        assertEquals(1, comments1.size());
+        assertEquals(" comment3 ", comments1.get(0).getData());
+    }
+
+    @Test public void textNodes() {
+        Document doc = Jsoup.parse("One<p>Two<a>Three</a><p>Four</p>Five");
+        List<TextNode> textNodes = doc.select("p").textNodes();
+        assertEquals(2, textNodes.size());
+        assertEquals("Two", textNodes.get(0).text());
+        assertEquals("Four", textNodes.get(1).text());
+    }
+
+    @Test public void dataNodes() {
+        Document doc = Jsoup.parse("<p>One</p><script>Two</script><style>Three</style>");
+        List<DataNode> dataNodes = doc.select("p, script, style").dataNodes();
+        assertEquals(2, dataNodes.size());
+        assertEquals("Two", dataNodes.get(0).getWholeData());
+        assertEquals("Three", dataNodes.get(1).getWholeData());
+
+        doc = Jsoup.parse("<head><script type=application/json><crux></script><script src=foo>Blah</script>");
+        Elements script = doc.select("script[type=application/json]");
+        List<DataNode> scriptNode = script.dataNodes();
+        assertEquals(1, scriptNode.size());
+        DataNode dataNode = scriptNode.get(0);
+        assertEquals("<crux>", dataNode.getWholeData());
+
+        // check if they're live
+        dataNode.setWholeData("<cromulent>");
+        assertEquals("<script type=\"application/json\"><cromulent></script>", script.outerHtml());
+    }
+
+    @Test public void nodesEmpty() {
+        Document doc = Jsoup.parse("<p>");
+        assertEquals(0, doc.select("form").textNodes().size());
+    }
+
+    @Test public void formElementsDescendButNotAccumulate() {
+        Document doc = Jsoup.parse("<div><div><form id=1>");
+        assertEquals(1, doc.select("div").forms().size());
     }
 
     @Test public void classWithHyphen() {

commit d89d9cb1a1ab3816cb6b31f986a79adb62e8f6c9

Fix Elements.forms()
the forms() method used to return FormElement-s that are directly in the Elements collection.
This behavior was broken since a657ae0240b875a703a1e4909d56ad59997d362d

Fixes #1384

src/main/java/org/jsoup/select/Elements.java
@@ -636,7 +636,11 @@ public class Elements extends ArrayList<Element> {
      * no forms.
      */
     public List<FormElement> forms() {
-        return nodesOfType(FormElement.class);
+        ArrayList<FormElement> forms = new ArrayList<>();
+        for (Element el: this)
+            if (el instanceof FormElement)
+                forms.add((FormElement) el);
+        return forms;
     }
 
     /**
@@ -644,7 +648,7 @@ public class Elements extends ArrayList<Element> {
      * @return Comment nodes, or an empty list if none.
      */
     public List<Comment> comments() {
-        return nodesOfType(Comment.class);
+        return childNodesOfType(Comment.class);
     }
 
     /**
@@ -652,7 +656,7 @@ public class Elements extends ArrayList<Element> {
      * @return TextNode nodes, or an empty list if none.
      */
     public List<TextNode> textNodes() {
-        return nodesOfType(TextNode.class);
+        return childNodesOfType(TextNode.class);
     }
 
     /**
@@ -661,20 +665,16 @@ public class Elements extends ArrayList<Element> {
      * @return Comment nodes, or an empty list if none.
      */
     public List<DataNode> dataNodes() {
-        return nodesOfType(DataNode.class);
+        return childNodesOfType(DataNode.class);
     }
 
-    private <T extends Node> List<T> nodesOfType(Class<T> tClass) {
+    private <T extends Node> List<T> childNodesOfType(Class<T> tClass) {
         ArrayList<T> nodes = new ArrayList<>();
         for (Element el: this) {
-            if (el.getClass().isInstance(tClass)) { // Handles FormElements
-                nodes.add(tClass.cast(el));
-            } else if (Node.class.isAssignableFrom(tClass)) { // check if child nodes match
-                for (int i = 0; i < el.childNodeSize(); i++) {
-                    Node node = el.childNode(i);
-                    if (tClass.isInstance(node))
-                        nodes.add(tClass.cast(node));
-                }
+            for (int i = 0; i < el.childNodeSize(); i++) {
+                Node node = el.childNode(i);
+                if (tClass.isInstance(node))
+                    nodes.add(tClass.cast(node));
             }
         }
         return nodes;

src/test/java/org/jsoup/select/ElementsTest.java
@@ -282,8 +282,8 @@ public class ElementsTest {
 
     @Test public void forms() {
         Document doc = Jsoup.parse("<form id=1><input name=q></form><div /><form id=2><input name=f></form>");
-        Elements els = doc.select("*");
-        assertEquals(9, els.size());
+        Elements els = doc.select("form, div");
+        assertEquals(3, els.size());
 
         List<FormElement> forms = els.forms();
         assertEquals(2, forms.size());
@@ -335,11 +335,6 @@ public class ElementsTest {
     @Test public void nodesEmpty() {
         Document doc = Jsoup.parse("<p>");
         assertEquals(0, doc.select("form").textNodes().size());
-    }
-
-    @Test public void formElementsDescendButNotAccumulate() {
-        Document doc = Jsoup.parse("<div><div><form id=1>");
-        assertEquals(1, doc.select("div").forms().size());
     }
 
     @Test public void classWithHyphen() {