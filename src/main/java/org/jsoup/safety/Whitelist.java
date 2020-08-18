0b181e5ba4b6e7667d3a2a7e69f2996e05239445
Matthew Reid <mreid@small-improvements.com>
2020-08-18 18:36:54 +0200

    Add deprecated Whitelist class for backwards compatibility

---
+++ src/main/java/org/jsoup/safety/Whitelist.java
@@ -0,0 +1,8 @@
+package org.jsoup.safety;
+
+/**
+ * This class is deprecated and will be removed in a future version. Please change your code to use the class {@link Allowlist} instead.
+ */
+@Deprecated
+public class Whitelist extends Allowlist {
+}
