8db060b72a22d2303f221663cd320a5c3f531e24
sebene <>
2021-03-04 12:31:58 +0100

    Issue #2 Fix Removes some stuff thatt was added by acccident

--- src/main/java/org/jsoup/examples/Wikipedia.java
+++ src/main/java/org/jsoup/examples/Wikipedia.java
@@ -3,13 +3,9 @@ package org.jsoup.examples;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.safety.Cleaner;
-import org.jsoup.safety.Safelist;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * A simple example, used on the jsoup website.
@@ -23,15 +19,6 @@ public class Wikipedia {
         for (Element headline : newsHeadlines) {
             log("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
         }
-        Cleaner cln = new Cleaner(new Safelist());
-        cln.trackDiscAttribs();
-        cln.trackDiscElems();
-        cln.clean(doc);
-
-        ArrayList<Node> elems = cln.getDiscElems();
-        ArrayList<Node> attribs = cln.getDiscAttribs();
-        log("Hej \n");
-
     }
 
     private static void log(String msg, String... vals) {
