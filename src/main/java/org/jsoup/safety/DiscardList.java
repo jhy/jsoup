e1e594edb3ffb82a4954ae047778aed73367d4f4
Eleonora B <eleonoraborzi@gmail.com>
2021-03-04 14:36:02 +0100

    Issue #1 Doc Adds documentation for DiscardList

--- src/main/java/org/jsoup/safety/DiscardList.java
+++ src/main/java/org/jsoup/safety/DiscardList.java
@@ -7,6 +7,12 @@ import org.jsoup.parser.Tag;
 
 import java.util.ArrayList;
 
+/**
+ * A container for DiscardList
+ *
+ * @author Sebastian Fagerlind sebene@kth.se and Eleonora Borzi borzi@kth.se
+ */
+
 public class DiscardList {
 
     private ArrayList<Node> discElems;
@@ -17,6 +23,10 @@ public class DiscardList {
     private int attribsMaxSize;
     private boolean trackDiscAttr;
 
+    /**
+     * Creates two ArrayLists, discElems and discAttribs. The lists are not tracked by default
+     * and the max size is set to -1 by default when the lists are not used.
+     */
     public DiscardList(){
         elemsMaxSize = -1;
         attribsMaxSize = -1;
@@ -25,11 +35,18 @@ public class DiscardList {
 
         trackDiscElems = false;
         trackDiscAttr = false;
-
     }
+    /**
+     * Set the max size of DiscElems
+     * @param elemsMaxSize
+     */
     public void setElemsMaxSize(int elemsMaxSize) {
         this.elemsMaxSize = elemsMaxSize;
     }
+    /**
+     * Set the max size of discAttribs
+     * @param attribsMaxSize
+     */
     public void setAttribsMaxSize(int attribsMaxSize) {
         this.attribsMaxSize = attribsMaxSize;
     }
@@ -45,11 +62,17 @@ public class DiscardList {
     public void stopAttribTracking(){
         trackDiscAttr = false;
     }
+    /**
+     * Adds the deleted tags to discElems list.
+     */
     public void addElem(Node elem){
         if(trackDiscElems && (elemsMaxSize == -1 || elemsMaxSize > discElems.size())){
             discElems.add(elem.clone());
         }
     }
+    /**
+     * Adds the deleted attributes to discAttribs list.
+     */
     public void addAttribute(Element elem, Attribute attr){
         if(trackDiscAttr && (attribsMaxSize == -1 || attribsMaxSize > discAttribs.size())){
             Element copy = new Element(Tag.valueOf(elem.tagName()), elem.baseUri(), null);
@@ -57,6 +80,10 @@ public class DiscardList {
             discAttribs.add(copy);
         }
     }
+
+    /**
+     * @return deep copy of discElems list
+     */
     public ArrayList<Node> getDiscElems(){
         ArrayList<Node> copy = new ArrayList<>(discElems.size());
         for (Node node:discElems){
@@ -64,6 +91,9 @@ public class DiscardList {
         }
         return copy;
     }
+    /**
+     * @return deep copy of discAttribs list
+     */
     public ArrayList<Node> getDiscAttribs(){
         ArrayList<Node> copy = new ArrayList<>(discAttribs.size());
         for (Node node:discAttribs){
@@ -71,5 +101,4 @@ public class DiscardList {
         }
         return copy;
     }
-
 }
