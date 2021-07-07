e718ddd847e15331d7d204a021b7f0aa80628a68
Jonathan Hedley <jonathan@hedley.net>
2021-07-07 15:34:57 +1000

    Testcase for Fux #1542 - but no repro yet

--- src/test/java/org/jsoup/integration/FuzzFixesTest.java
+++ src/test/java/org/jsoup/integration/FuzzFixesTest.java
@@ -26,19 +26,31 @@ public class FuzzFixesTest {
     }
 
     @Test public void resetInsertionMode() throws IOException {
+        // https://github.com/jhy/jsoup/issues/1538
         File in = ParseTest.getFile("/fuzztests/1538.html"); // lots of escape chars etc.
         Document doc = Jsoup.parse(in, "UTF-8");
         assertNotNull(doc);
     }
 
     @Test public void xmlDeclOverflow() throws IOException {
+        // https://github.com/jhy/jsoup/issues/1539
         File in = ParseTest.getFile("/fuzztests/1539.html"); // lots of escape chars etc.
         Document doc = Jsoup.parse(in, "UTF-8");
         assertNotNull(doc);
 
         Document docXml = Jsoup.parse(new FileInputStream(in), "UTF-8", "https://example.com", Parser.xmlParser());
         assertNotNull(docXml);
+    }
 
+    @Test public void unconsume() throws IOException {
+        // https://github.com/jhy/jsoup/issues/1542
+        File in = ParseTest.getFile("/fuzztests/1542.html"); // lots of escape chars etc.
+        Document doc = Jsoup.parse(in, "UTF-8");
+        assertNotNull(doc);
 
+        Document docFromString = Jsoup.parse(ParseTest.getFileAsString(in));
+        assertNotNull(docFromString);
+
+        // I haven't been able to replicate this - per code, content is handed as a plain string, as here. File encoding issue? Tried UTF8, ascii, windows-1252.
     }
 }
