1c65ebf3ceffa8353e320ae823ba7769d54bb7d2
Meodinger Wang <54493746+Meodinger@users.noreply.github.com>
2022-04-19 14:26:49 +0800

    Update module-info.java

--- src/main/moditect/module-info.java
+++ src/main/moditect/module-info.java
@@ -1,4 +1,9 @@
 module org.jsoup {
-    requires java.xml;
+    requires static java.xml;
+
     exports org.jsoup;
+    exports org.jsoup.nodes;
+    exports org.jsoup.parser;
+    exports org.jsoup.safety;
+    exports org.jsoup.select;
 }
