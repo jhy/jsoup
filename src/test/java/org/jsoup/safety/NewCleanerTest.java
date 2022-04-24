a4a139cca5066f772c9c6c71160e00b7f97daf6c
FKBugMaker <56469033+FKBugMaker@users.noreply.github.com>
2022-04-24 21:48:36 +0800

    Update NewCleanerTest.java
    add the Javadoc and modify the safelist

--- src/test/java/org/jsoup/safety/NewCleanerTest.java
+++ src/test/java/org/jsoup/safety/NewCleanerTest.java
@@ -1,36 +1,29 @@
 package org.jsoup.safety;
-
 import org.jsoup.Jsoup;
 import org.jsoup.TextUtil;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Tests for the cleaner.
+ * @author FKBugMaker
+ */
 public class NewCleanerTest {
-    /*
-    Test the case which is described as the issue #1731
-     */
     @Test public void issueTest() {
         String html = "<a href=\"invalid link\">my link</a>";
-        String cleanHtml = Jsoup.clean(html, Safelist.simpleText());
+        String cleanHtml = Jsoup.clean(html, Safelist.basic());
         assertEquals("my link", TextUtil.stripNewlines(cleanHtml));
     }
 
-    /*
-    Test the case of empty link name between <a> and </a>
-     */
     @Test public void emptyTest() {
         String html = "<a href='http://evil.com'></a>";
-        String cleanHtml = Jsoup.clean(html, Safelist.simpleText());
+        String cleanHtml = Jsoup.clean(html, Safelist.basic());
         assertEquals("", TextUtil.stripNewlines(cleanHtml));
     }
 
-    /*
-    Test the case of multiple groups of <a> and </a>
-     */
     @Test public void multipleLinkNameTest() {
         String html = "<a>One</a> <a href>Two</a> <a>three</a> <a href>four</a>";
-        String cleanHtml = Jsoup.clean(html, Safelist.simpleText());
+        String cleanHtml = Jsoup.clean(html, Safelist.basic());
         assertEquals("One Two three four", TextUtil.stripNewlines(cleanHtml));
     }
 }
