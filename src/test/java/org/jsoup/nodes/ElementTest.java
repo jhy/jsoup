jhy/jsoup

commit f4d00c290325935186f75f1206d16397cc157ced

Simplified TextNode.outerHtml a little

src/main/java/org/jsoup/nodes/TextNode.java
@@ -74,20 +74,22 @@ public class TextNode extends LeafNode {
         String tail = text.substring(offset);
         text(head);
         TextNode tailNode = new TextNode(tail);
-        if (parent() != null)
-            parent().addChildren(siblingIndex()+1, tailNode);
+        if (parentNode != null)
+            parentNode.addChildren(siblingIndex()+1, tailNode);
 
         return tailNode;
     }
 
 	void outerHtmlHead(Appendable accum, int depth, Document.OutputSettings out) throws IOException {
         final boolean prettyPrint = out.prettyPrint();
-        Element parent = parentNode instanceof Element ? ((Element) parentNode) : null;
-        boolean parentIndent = parent != null && parent.shouldIndent(out);
-        if (parentIndent && getWholeText().startsWith("\n") && isBlank()) // we are skippable whitespace
+        final Element parent = parentNode instanceof Element ? ((Element) parentNode) : null;
+        final boolean parentIndent = parent != null && parent.shouldIndent(out);
+        final boolean blank = isBlank();
+
+        if (parentIndent && StringUtil.startsWithNewline(coreValue()) && blank) // we are skippable whitespace
             return;
 
-        if (prettyPrint && ((siblingIndex() == 0 && parent != null && parent.tag().formatAsBlock() && !isBlank()) || (out.outline() && siblingNodes().size()>0 && !isBlank()) ))
+        if (prettyPrint && ((siblingIndex == 0 && parent != null && parent.tag().formatAsBlock() && !blank) || (out.outline() && siblingNodes().size()>0 && !blank) ))
             indent(accum, depth, out);
 
         final boolean normaliseWhite = prettyPrint && !Element.preserveWhitespace(parentNode);

src/main/java/org/jsoup/internal/StringUtil.java
@@ -147,7 +147,7 @@ public final class StringUtil {
      * @param string string to test
      * @return if string is blank
      */
-    public static boolean isBlank(String string) {
+    public static boolean isBlank(final String string) {
         if (string == null || string.length() == 0)
             return true;
 
@@ -157,6 +157,17 @@ public final class StringUtil {
                 return false;
         }
         return true;
+    }
+
+    /**
+     Tests if a string starts with a newline character
+     @param string string to test
+     @return if its first character is a newline
+     */
+    public static boolean startsWithNewline(final String string) {
+        if (string == null || string.length() == 0)
+            return false;
+        return string.charAt(0) == '\n';
     }
 
     /**

src/main/java/org/jsoup/nodes/LeafNode.java
@@ -38,9 +38,8 @@ abstract class LeafNode extends Node {
 
     @Override
     public String attr(String key) {
-        Validate.notNull(key);
         if (!hasAttributes()) {
-            return key.equals(nodeName()) ? (String) value : EmptyString;
+            return nodeName().equals(key) ? (String) value : EmptyString;
         }
         return super.attr(key);
     }

commit f6d9aa050e3d58b6454d4eaef00f1d061f368eb4

Don't skip blanks in preserveWhiteSpace descenders
Fixes #1776.

Regressed in f4d00c290325935186f75f1206d16397cc157ced

src/main/java/org/jsoup/nodes/TextNode.java
@@ -85,14 +85,14 @@ public class TextNode extends LeafNode {
         final Element parent = parentNode instanceof Element ? ((Element) parentNode) : null;
         final boolean parentIndent = parent != null && parent.shouldIndent(out);
         final boolean blank = isBlank();
+        final boolean normaliseWhite = prettyPrint && !Element.preserveWhitespace(parentNode);
 
-        if (parentIndent && StringUtil.startsWithNewline(coreValue()) && blank) // we are skippable whitespace
+        if (normaliseWhite && parentIndent && StringUtil.startsWithNewline(coreValue()) && blank) // we are skippable whitespace
             return;
 
         if (prettyPrint && ((siblingIndex == 0 && parent != null && parent.tag().formatAsBlock() && !blank) || (out.outline() && siblingNodes().size()>0 && !blank) ))
             indent(accum, depth, out);
 
-        final boolean normaliseWhite = prettyPrint && !Element.preserveWhitespace(parentNode);
         final boolean stripWhite = prettyPrint && parentNode instanceof Document;
         Entities.escape(accum, coreValue(), out, false, normaliseWhite, stripWhite);
     }

src/test/java/org/jsoup/nodes/ElementTest.java
@@ -2216,4 +2216,42 @@ public class ElementTest {
         assertEquals("Hello World", p.text());
         assertEquals("Hello\nWorld", p.wholeText());
     }
+
+    @Test void preformatFlowsToChildTextNodes() {
+        // https://github.com/jhy/jsoup/issues/1776
+        String html = "<div><pre>One\n<span>\nTwo</span>\n <span>  \nThree</span>\n <span>Four <span>Five</span>\n  Six\n</pre>";
+        Document doc = Jsoup.parse(html);
+        doc.outputSettings().indentAmount(2).prettyPrint(true);
+
+        Element div = doc.selectFirst("div");
+        assertNotNull(div);
+        String actual = div.outerHtml();
+        String expect = "<div>\n" +
+            "  <pre>One\n" +
+            "<span>\n" +
+            "Two</span>\n" +
+            " <span>  \n" +
+            "Three</span>\n" +
+            " <span>Four <span>Five</span>\n" +
+            "  Six\n" +
+            "</span></pre>\n" +
+            "</div>";
+        assertEquals(expect, actual);
+
+        String expectText = "One\n" +
+            "\n" +
+            "Two\n" +
+            "   \n" +
+            "Three\n" +
+            " Four Five\n" +
+            "  Six\n";
+        assertEquals(expectText, div.wholeText());
+
+        String expectOwn = "One\n" +
+            "\n" +
+            " \n" +
+            " ";
+        assertEquals(expectOwn, div.child(0).wholeOwnText());
+
+    }
 }