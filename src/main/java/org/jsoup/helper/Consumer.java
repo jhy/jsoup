15bf11a8a919fcce6dbbdccddb48b74e94ab7dc3
Isira Seneviratne <isirasen96@gmail.com>
2022-12-23 08:48:08 +0530

    Use Java 8 Consumer.

--- src/main/java/org/jsoup/helper/Consumer.java
+++ src/main/java/org/jsoup/helper/Consumer.java
@@ -3,14 +3,17 @@ package org.jsoup.helper;
 /**
  A functional interface (ala Java's {@link java.util.function.Consumer} interface, implemented here for cross compatibility with Android.
  @param <T> the input type
- */
-@FunctionalInterface
-public interface Consumer<T> {
 
+ @deprecated This will be removed in favor of using {@link java.util.function.Consumer} instead in the next release.
+ */
+@Deprecated
+@FunctionalInterface
+public interface Consumer<T> extends java.util.function.Consumer<T> {
     /**
      * Execute this operation on the supplied argument. It is expected to have side effects.
      *
      * @param t the input argument
      */
+    @Override
     void accept(T t);
 }
