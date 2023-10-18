8a597925a7dae0b3bce86b1b5638699abed72331
Jonathan Hedley <jonathan@hedley.net>
2023-10-18 13:18:37 +1100

    Make isSafe* public
    Fixes #1780

--- src/main/java/org/jsoup/safety/Safelist.java
+++ src/main/java/org/jsoup/safety/Safelist.java
@@ -522,22 +522,22 @@ public class Safelist {
     }
 
     /**
-     * Test if the supplied tag is allowed by this safelist
+     * Test if the supplied tag is allowed by this safelist.
      * @param tag test tag
      * @return true if allowed
      */
-    protected boolean isSafeTag(String tag) {
+    public boolean isSafeTag(String tag) {
         return tagNames.contains(TagName.valueOf(tag));
     }
 
     /**
-     * Test if the supplied attribute is allowed by this safelist for this tag
+     * Test if the supplied attribute is allowed by this safelist for this tag.
      * @param tagName tag to consider allowing the attribute in
      * @param el element under test, to confirm protocol
      * @param attr attribute under test
      * @return true if allowed
      */
-    protected boolean isSafeAttribute(String tagName, Element el, Attribute attr) {
+    public boolean isSafeAttribute(String tagName, Element el, Attribute attr) {
         TagName tag = TagName.valueOf(tagName);
         AttributeKey key = AttributeKey.valueOf(attr.getKey());
 
