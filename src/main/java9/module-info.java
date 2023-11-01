882bb9ce8a799657d8b02894dbdb5b42b1e3d215
Jonathan Hedley <jonathan@hedley.net>
2023-11-01 15:39:07 +1100

    Repackaged the library with native (vs automatic) JPMS module support
    Fixes #1943
    Fixes #1466

---
+++ src/main/java9/module-info.java
@@ -0,0 +1,10 @@
+module org.jsoup {
+    exports org.jsoup;
+    exports org.jsoup.helper;
+    exports org.jsoup.nodes;
+    exports org.jsoup.parser;
+    exports org.jsoup.safety;
+    exports org.jsoup.select;
+
+    requires transitive java.xml; // for org.w3c.dom out of W3CDom
+}
