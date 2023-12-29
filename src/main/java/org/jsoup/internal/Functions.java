04e683d48c0e8ecdb0f3733f63cdbe59a908b583
Isira Seneviratne <isirasen96@gmail.com>
2023-12-29 08:18:55 +0530

    Add constant function variables

--- src/main/java/org/jsoup/internal/Functions.java
+++ src/main/java/org/jsoup/internal/Functions.java
@@ -7,22 +7,27 @@ import java.util.function.Function;
  * An internal class containing functions for use with {@link Map#computeIfAbsent(Object, Function)}.
  */
 public class Functions {
+    private static final Function LIST_FUNCTION = key -> new ArrayList<>();
+    private static final Function SET_FUNCTION = key -> new HashSet<>();
+    private static final Function MAP_FUNCTION = key -> new HashMap<>();
+    private static final Function IDENTITY_MAP_FUNCTION = key -> new IdentityHashMap<>();
+
     private Functions() {
     }
 
     public static <T, U> Function<T, List<U>> listFunction() {
-        return key -> new ArrayList<>();
+        return (Function<T, List<U>>) LIST_FUNCTION;
     }
 
     public static <T, U> Function<T, Set<U>> setFunction() {
-        return key -> new HashSet<>();
+        return (Function<T, Set<U>>) SET_FUNCTION;
     }
 
     public static <T, K, V> Function<T, Map<K, V>> mapFunction() {
-        return key -> new HashMap<>();
+        return (Function<T, Map<K, V>>) MAP_FUNCTION;
     }
 
     public static <T, K, V> Function<T, IdentityHashMap<K, V>> identityMapFunction() {
-        return key -> new IdentityHashMap<>();
+        return (Function<T, IdentityHashMap<K, V>>) IDENTITY_MAP_FUNCTION;
     }
 }
