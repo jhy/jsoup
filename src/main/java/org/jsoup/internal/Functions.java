5065b1f199cc6069cc36c99c3e4711ad7708bd85
Jonathan Hedley <jonathan@hedley.net>
2023-12-30 11:52:54 +1100

    Static field style

--- src/main/java/org/jsoup/internal/Functions.java
+++ src/main/java/org/jsoup/internal/Functions.java
@@ -14,27 +14,27 @@ import java.util.function.Function;
  */
 @SuppressWarnings({"rawtypes", "unchecked"})
 public class Functions {
-    private static final Function LIST_FUNCTION = key -> new ArrayList<>();
-    private static final Function SET_FUNCTION = key -> new HashSet<>();
-    private static final Function MAP_FUNCTION = key -> new HashMap<>();
-    private static final Function IDENTITY_MAP_FUNCTION = key -> new IdentityHashMap<>();
+    private static final Function ListFunction = key -> new ArrayList<>();
+    private static final Function SetFunction = key -> new HashSet<>();
+    private static final Function MapFunction = key -> new HashMap<>();
+    private static final Function IdentityMapFunction = key -> new IdentityHashMap<>();
 
     private Functions() {
     }
 
     public static <T, U> Function<T, List<U>> listFunction() {
-        return (Function<T, List<U>>) LIST_FUNCTION;
+        return (Function<T, List<U>>) ListFunction;
     }
 
     public static <T, U> Function<T, Set<U>> setFunction() {
-        return (Function<T, Set<U>>) SET_FUNCTION;
+        return (Function<T, Set<U>>) SetFunction;
     }
 
     public static <T, K, V> Function<T, Map<K, V>> mapFunction() {
-        return (Function<T, Map<K, V>>) MAP_FUNCTION;
+        return (Function<T, Map<K, V>>) MapFunction;
     }
 
     public static <T, K, V> Function<T, IdentityHashMap<K, V>> identityMapFunction() {
-        return (Function<T, IdentityHashMap<K, V>>) IDENTITY_MAP_FUNCTION;
+        return (Function<T, IdentityHashMap<K, V>>) IdentityMapFunction;
     }
 }
