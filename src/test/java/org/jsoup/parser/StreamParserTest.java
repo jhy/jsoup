5646eb58dee47bc06207dcf8d127c9f7d00a3cd6
Jonathan Hedley <jonathan@hedley.net>
2024-01-01 16:55:13 +1100

    Test stream order

--- src/test/java/org/jsoup/parser/StreamParserTest.java
+++ src/test/java/org/jsoup/parser/StreamParserTest.java
@@ -2,6 +2,8 @@ package org.jsoup.parser;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 
@@ -13,17 +15,30 @@ class StreamParserTest {
 
     @Test
     void stream() {
-        String html = "<title>Test</title></head><div>D1</div><div>D2<p>P One</p><p>P Two</p></div><div>D3<p>P three</p>";
-
+        String html = "<title>Test</title></head><div id=1>D1</div><div id=2>D2<p id=3><span>P One</p><p id=4>P Two</p></div><div id=5>D3<p id=6>P three</p>";
         StreamParser parser = new StreamParser(Parser.htmlParser()).parse(html, "");
 
-        // todo - actual tests
-        parser.stream().forEach(element -> System.out.println(element.nodeName() + ": " + element.ownText() + ", " + element.nextElementSibling()));
+        StringBuilder seen = new StringBuilder();
+        parser.stream().forEachOrdered(el -> trackSeen(el, seen));
+        assertEquals("title[Test];head+;div#1[D1]+;span[P One];p#3+;p#4[P Two];div#2[D2]+;p#6[P three];div#5[D3];body;html;", seen.toString());
+        // checks expected order, and the + indicates that element had a next sibling at time of emission
+    }
+
+    static void trackSeen(Element el, StringBuilder actual) {
+            actual.append(el.tagName());
+            if (el.hasAttr("id"))
+                actual.append("#").append(el.id());
+            if (!el.ownText().isEmpty())
+                actual.append("[").append(el.ownText()).append("]");
+            if (el.nextElementSibling() != null)
+                actual.append("+");
+
+        actual.append(";");
     }
 
     @Test
     void select() {
-        String html = "<title>One</title><p>P One</p><p>P Two</p>";
+        String html = "<title>One</title><p id=1>P One</p><p id=2>P Two</p>";
         StreamParser parser = new StreamParser(Parser.htmlParser()).parse(html, "");
 
         Element title = parser.selectFirst("title").get();
