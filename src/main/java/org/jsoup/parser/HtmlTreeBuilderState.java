jhy/jsoup

commit 9c437b4b961c6ed8a52ccb0a1be58de51f2e1e57

script in svg should be parsed as ScriptData
And added support for custom tags as data when in foreign context namespace.

Fixes #2320

src/main/java/org/jsoup/parser/HtmlTreeBuilderState.java
@@ -1805,9 +1805,22 @@ enum HtmlTreeBuilderState {
 
                     // Any other start:
                     // (whatwg says to fix up tag name and attribute case per a table - we will preserve original case instead)
-                    tb.insertForeignElementFor(start, tb.currentElement().tag().namespace());
+                    String namespace = tb.currentElement().tag().namespace();
+                    tb.insertForeignElementFor(start, namespace);
                     // (self-closing handled in insert)
                     // if self-closing svg script -- level and execution elided
+
+                    // seemingly not in spec, but as browser behavior, get into ScriptData state for svg script; and allow custom data tags
+                    TokeniserState textState = tb.tagFor(start.tagName.value(), start.normalName, namespace, tb.settings).textState();
+                    if (textState != null) {
+                        if (start.normalName.equals("script"))
+                            tb.tokeniser.transition(TokeniserState.ScriptData);
+                        else
+                            tb.tokeniser.transition(textState);
+                        tb.markInsertionMode();
+                        tb.transition(Text);
+                    }
+
                     break;
 
                 case EndTag:

src/main/java/org/jsoup/parser/HtmlTreeBuilder.java
@@ -323,7 +323,7 @@ public class HtmlTreeBuilder extends TreeBuilder {
             new Element(tag, null, attributes);
     }
 
-    /** Inserts an HTML element for the given tag) */
+    /** Inserts an HTML element for the given tag */
     Element insertElementFor(final Token.StartTag startTag) {
         Element el = createElementFor(startTag, NamespaceHtml, false);
         doInsertElement(el);

src/main/java/org/jsoup/parser/TagSet.java
@@ -206,6 +206,7 @@ public class TagSet {
         String[] inlineMathTags = {"mi", "mo", "msup", "mn", "mtext"};
         String[] blockSvgTags = {"svg", "femerge", "femergenode"}; // note these are LC versions, but actually preserve case
         String[] inlineSvgTags = {"text"};
+        String[] dataSvgTags = {"script"};
 
         return new TagSet()
             .setupTags(NamespaceHtml, blockTags, tag -> tag.set(Tag.Block))
@@ -220,6 +221,7 @@ public class TagSet {
             .setupTags(NamespaceMathml, inlineMathTags, tag -> tag.set(0))
             .setupTags(NamespaceSvg, blockSvgTags, tag -> tag.set(Tag.Block))
             .setupTags(NamespaceSvg, inlineSvgTags, tag -> tag.set(0))
+            .setupTags(NamespaceSvg, dataSvgTags, tag -> tag.set(Tag.Data))
             ;
     }
 

src/main/java/org/jsoup/safety/Cleaner.java
@@ -156,9 +156,9 @@ public class Cleaner {
                 TextNode destText = new TextNode(sourceText.getWholeText());
                 destination.appendChild(destText);
             } else if (source instanceof DataNode && safelist.isSafeTag(source.parent().normalName())) {
-              DataNode sourceData = (DataNode) source;
-              DataNode destData = new DataNode(sourceData.getWholeData());
-              destination.appendChild(destData);
+                DataNode sourceData = (DataNode) source;
+                DataNode destData = new DataNode(sourceData.getWholeData());
+                destination.appendChild(destData);
             } else { // else, we don't care about comments, xml proc instructions, etc
                 numDiscarded++;
             }

src/test/java/org/jsoup/nodes/ElementTest.java
@@ -3182,4 +3182,14 @@ public class ElementTest {
         Elements all = div.select("div");
         assertSelectedIds(all, "out", "1", "2", "3");
     }
+
+    @Test void setTextOnSvgScriptSetsDataNode() {
+        // calling .text() on svg script will create a datanode, as defined in TagSet
+        String html = "<svg><script></script></svg>";
+        Document doc = Jsoup.parse(html);
+        Element script = doc.expectFirst("script");
+        script.text("a < b");
+        assertEquals("<script>a < b</script>", script.outerHtml()); // not encoded
+        assertEquals("a < b", script.data());
+    }
 }

src/test/java/org/jsoup/parser/HtmlParserTest.java
@@ -2089,4 +2089,29 @@ public class HtmlParserTest {
         doc.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
         assertEquals("<div /><custom /><custom>Foo</custom>", TextUtil.stripNewlines(doc.body().html()));
     }
+
+    @Test void svgScriptParsedAsScriptData() {
+        // https://github.com/jhy/jsoup/issues/2320
+        String html = "<svg><script>a < b</script></svg>";
+        Document doc = Jsoup.parse(html);
+        Element script = doc.expectFirst("script");
+        assertEquals(Parser.NamespaceSvg, script.tag().namespace);
+        assertTrue(script.tag().is(Tag.Data));
+
+        DataNode data = (DataNode) script.childNode(0);
+        assertEquals("a < b", data.getWholeData());
+    }
+
+    @Test void allowCustomDataInForeignElements() {
+        Tag dataTag = new Tag("data", Parser.NamespaceSvg);
+        dataTag.set(Tag.Data);
+        TagSet tagSet = TagSet.HtmlTagSet.add(dataTag);
+        String html = "<svg><data>a < b</data></svg>";
+        Document doc = Jsoup.parse(html, Parser.htmlParser().tagSet(tagSet));
+        Element data = doc.expectFirst("data");
+        assertEquals(Parser.NamespaceSvg, data.tag().namespace);
+        assertEquals("", data.text());
+        assertEquals("a < b", data.data());
+        assertEquals("<data>a < b</data>", data.outerHtml());
+    }
 }

src/test/java/org/jsoup/safety/CleanerTest.java
@@ -472,4 +472,11 @@ public class CleanerTest {
         assertEquals("<a href=\"http://external.com/\" rel=\"nofollow\">One</a> <a href=\"/relative/\">Two</a> <a href=\"../other/\">Three</a> <a href=\"http://example.com/bar\" rel=\"nofollow\">Four</a>", clean4);
     }
 
+    @Test void discardsSvgScriptData() {
+        // https://github.com/jhy/jsoup/issues/2320
+        Safelist svgOk = Safelist.none().addTags("svg");
+        String cleaned = Jsoup.clean("<svg><script> a < b </script></svg>", svgOk);
+        assertEquals("<svg></svg>", cleaned);
+    }
+
 }

commit e3e22ad52b910abf673e5c2929d4565633c728c0

Force breakout of Text state at EOF
The original marked state might have been Text in the case of rudely nested script in SVG, in which case we weren't exiting the Text state, and would overflow.

That path was caused by the change in 9c437b4b961c6ed8a52ccb0a1be58de51f2e1e57; not in any release.

src/main/java/org/jsoup/parser/HtmlTreeBuilderState.java
@@ -990,6 +990,8 @@ enum HtmlTreeBuilderState {
                 // if current node is script: already started
                 tb.pop();
                 tb.transition(tb.originalState());
+                if (tb.state() == Text) // stack is such that we couldn't transition out; just close
+                    tb.transition(InBody);
                 return tb.process(t);
             } else if (t.isEndTag()) {
                 // if: An end tag whose tag name is "script" -- scripting nesting level, if evaluating scripts