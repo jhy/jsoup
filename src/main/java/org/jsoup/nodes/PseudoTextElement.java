b62e362d3945e86725c817101332b66277aff9e4
Jonathan Hedley <jonathan@hedley.net>
2025-07-01 15:30:07 +1000

    Fix annotation typo

--- src/main/java/org/jsoup/nodes/PseudoTextElement.java
+++ src/main/java/org/jsoup/nodes/PseudoTextElement.java
@@ -6,7 +6,7 @@ import org.jsoup.parser.Tag;
 /**
  * Represents a {@link TextNode} as an {@link Element}, to enable text nodes to be selected with
  * the {@link org.jsoup.select.Selector} {@code :matchText} syntax.
- * @Deprecated use {@link Element#selectNodes(String, Class)} instead, with selector of <code>::textnode</code> and class <code>TextNode</code>.
+ * @deprecated use {@link Element#selectNodes(String, Class)} instead, with selector of <code>::textnode</code> and class <code>TextNode</code>.
  */
 @Deprecated
 public class PseudoTextElement extends Element {
